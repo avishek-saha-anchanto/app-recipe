680dd29688ec1188dd53a0b83564bcc50ef5f9a4
Avishek <avishek.kdh@gmail.com>
2024-03-02 12:35:21 +0530

    recipe and shopping component

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,11 @@
+export class Indgredient
+{
+    public name:string;
+    public amount:number;
+
+    constructor(name,amount)
+    {
+        this.name=name;
+        this.amount=amount;
+    }
+}
