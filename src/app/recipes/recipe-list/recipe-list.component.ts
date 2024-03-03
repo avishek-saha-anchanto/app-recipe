26d1cd62e5e19f0b0a04bbf34fb5a7a0b892e7d6
Avishek <avishek.kdh@gmail.com>
2024-03-04 01:07:55 +0530

    dropDown directive

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,8 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes :Recipe[] = [
-    new Recipe('A test recipe', 'this is a very tastful recipe','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('A test recipe', 'this is a very tastful recipe','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('A test recipe1', 'this is a very tastful recipe','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('A test recipe2', 'this is a very tastful recipe','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
 
   ];
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
